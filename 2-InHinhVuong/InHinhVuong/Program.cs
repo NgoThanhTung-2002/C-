28792ad2a1f71859826b74d77400afbca6b5dc39
NgoThanhTung-2002 <86903392+NgoThanhTung-2002@users.noreply.github.com>
2022-09-18 11:29:02 +0700

    commit code bai 2

---
+++ 2-InHinhVuong/InHinhVuong/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InHinhVuong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
